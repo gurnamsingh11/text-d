ec1f0b7736f4ff63bb28ca7ffd00c3404dbb700c
Gurnam <lohiagurunam11@gmail.com>
2024-08-12 23:30:17 +0530

    Update gs.py

--- core/gs.py
+++ core/gs.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from diff_gaussian_rasterization import (
+from fast_gauss import (
     GaussianRasterizationSettings,
     GaussianRasterizer,
 )
